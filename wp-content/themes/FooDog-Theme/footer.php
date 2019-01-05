889c030f947b14d293b5cc709b846cd0fb9c8a5c
JustineCrenier <justine.crenier@gmail.com>
2019-01-05 20:24:16 +0100

    footer in progress

--- wp-content/themes/FooDog-Theme/footer.php
+++ wp-content/themes/FooDog-Theme/footer.php
@@ -1,5 +1,30 @@
-	<footer>
-		
+	<footer class="container-fluid footer">
+		<div class="row main-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-4 d-flex flex-column align-items-center">
+						<p class="titre-footer">Catégories</p>
+						<?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
+					</div>
+					<div class="col-4 d-flex flex-column align-items-center">
+						deux
+					</div>
+					<div class="col-4 d-flex flex-column align-items-center">
+						trois
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row social">
+			<!-- Mettre les réseaux ici -->
+			<div class="container">
+				<div class="row">
+					<div class="col-12 d-flex justify-content-center">
+						facebook Twitter Instagram
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 	<!-- Script -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- wp-content/themes/FooDog-Theme/functions.php
+++ wp-content/themes/FooDog-Theme/functions.php
@@ -2,7 +2,7 @@
 
 register_nav_menus( array(
     'main-menu' => 'Menu principal',
-    'second-menu' => 'Second menu',
+    'footer-menu' => 'Footer menu',
 ));
 
 add_theme_support('post-thumbnails');
